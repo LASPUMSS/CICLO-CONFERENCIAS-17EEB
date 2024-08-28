03fad4fe275cefb8751d7265adb288524a01064d
LASPUMSS <umssluisalbertosolisp@gmail.com>
2024-08-28 10:20:37 -0600

    UPDATE

---
+++ inst/requeriments.R
@@ -0,0 +1,42 @@
+# 1. MIDAS:
+
+install.packages('midasr', dependencies = T)
+install.packages('data.table', dependencies = T)
+install.packages('dplyr', dependencies = T)
+install.packages('ggplot2', dependencies = T)
+install.packages('ggfortify', dependencies = T)
+
+# 2. Regresiones Ridge:
+
+install.packages('glmnet', dependencies = T)
+install.packages('psych', dependencies = T)
+install.packages('caret', dependencies = T)
+
+# 3. Regression Trees y Random Forest
+
+install.packages('rsample', dependencies = T)
+install.packages('randomForest', dependencies = T)
+install.packages('ranger', dependencies = T)
+install.packages('h2o', dependencies = T)
+install.packages('AmesHousing', dependencies = T)
+install.packages('rpart', dependencies = T)
+install.packages('rpart.plot', dependencies = T)
+install.packages('tibble', dependencies = T)
+install.packages('rugarch', dependencies = T)
+
+# 4. Boosting
+
+install.packages('gbm', dependencies = T)
+install.packages('xgboost', dependencies = T)
+install.packages('pdp', dependencies = T)
+install.packages('lime', dependencies = T)
+
+# 5.Redes Neuronales
+
+install.packages('neuralnet', dependencies = T)
+install.packages('MASS', dependencies = T)
+
+# 6. Evaluación de Pronóstico
+
+install.packages('multDM', dependencies = T)
+install.packages('MCS', dependencies = T)
